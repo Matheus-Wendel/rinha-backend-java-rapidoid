30bb6e82221c5b2aa654db651777887c6027dea8
Matheus Wendel <matheus_wendel@outlook.com>
2023-10-08 20:16:37 -0300

    cira classe generica

---
+++ src/main/java/com/matheus/app/Error.java
@@ -0,0 +1,20 @@
+package com.matheus.app;
+
+public class Error {
+    
+    public String erro;
+
+
+    public String getErro() {
+        return this.erro;
+    }
+
+    public void setErro(String erro) {
+        this.erro = erro;
+    }
+
+    public Error(String erro) {
+        this.erro = erro;
+    }
+
+}
