9a7f8843e85c4eca4993219464b4890e48d6920f
Matheus Wendel <matheus_wendel@outlook.com>
2023-10-08 20:16:11 -0300

    adiciona index criacao tabelas

--- src/main/java/com/matheus/app/domain/Pessoa.java
+++ src/main/java/com/matheus/app/domain/Pessoa.java
@@ -8,15 +8,24 @@ import java.util.UUID;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.apache.commons.validator.GenericValidator;
+import org.hibernate.annotations.Type;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.matheus.app.ForceStringDeserializer;
 
 @Entity
+@Table(indexes = {
+        @Index(columnList = "id"),
+        @Index(columnList = "apelido"),
+        @Index(columnList = "nome"),
+        @Index(columnList = "stack")
+})
 public class Pessoa {
 
     /**
@@ -25,6 +34,7 @@ public class Pessoa {
     private static final String FORMATO_DATA = "YYYY-MM-DD";
 
     @Id
+    @Type(type = "pg-uuid")
     public UUID id;
 
     @NotNull
@@ -40,7 +50,6 @@ public class Pessoa {
     @NotNull
     @JsonDeserialize(using = ForceStringDeserializer.class)
     public String nascimento;
-    @NotNull
     @Column(nullable = true, length = 100)
     private String stack;
 
@@ -62,10 +71,7 @@ public class Pessoa {
 
     public boolean validate() {
 
-        if (!GenericValidator.isDate(nascimento, FORMATO_DATA, true)) {
-            return false;
-        }
-        return true;
+        return GenericValidator.isDate(nascimento, FORMATO_DATA, true);
     }
 
     public UUID getId() {
@@ -99,5 +105,17 @@ public class Pessoa {
     public void setNascimento(String nascimento) {
         this.nascimento = nascimento;
     }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", apelido='" + getApelido() + "'" +
+            ", nome='" + getNome() + "'" +
+            ", nascimento='" + getNascimento() + "'" +
+            ", stack='" + getStack() + "'" +
+            "}";
+    }
+
 
 }
